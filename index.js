e6e925bec0c91665f3e57f143d576a69ff1e2444
galihpikun <galihrakha003@gmail.com>
2026-02-26 10:28:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import routeTask from "./routes/routeTask.js";
+import cors from "cors"
+
+const app = express();
+const port = 3000;
+
+// app.use(cors());
+app.use(express.json());
+
+app.use('/task', routeTask)
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
